c8cdf0d19cb725707c3f7de1a309f561203ca912
weixsong <watkinsong@163.com>
2016-01-07 18:57:20 +0800

    support parameter for elasticlunr.tokenizer

--- release/elasticlunr.js
+++ release/elasticlunr.js
@@ -222,9 +222,14 @@ elasticlunr.EventEmitter.prototype.hasHandler = function (name) {
 /**
  * A function for splitting a string into tokens.
  * Currently English is support as default.
+ * Uses `elasticlunr.tokenizer.seperator` to split strings, you could change
+ * the value of this property to set how you want strings are split into tokens.
+ * IMPORTANT: use elasticlunr.tokenizer.seperator carefully, if you are not familiar with
+ * text process, then you'd better not change it.
  *
  * @module
  * @param {String} str The string that you want to tokenize.
+ * @see elasticlunr.tokenizer.seperator
  * @return {Array}
  */
 elasticlunr.tokenizer = function (obj) {
@@ -235,9 +240,17 @@ elasticlunr.tokenizer = function (obj) {
     });
   }
 
-  return obj.toString().trim().toLowerCase().split(/[\s\-]+/);
+  return obj.toString().trim().toLowerCase().split(elasticlunr.tokenizer.seperator);
 };
 
+/**
+ * The sperator used to split a string into tokens. Override this property to change the behaviour of
+ * `elasticlunr.tokenizer` behaviour when tokenizing strings. By default this splits on whitespace and hyphens.
+ *
+ * @static
+ * @see elasticlunr.tokenizer
+ */
+elasticlunr.tokenizer.seperator = /[\s\-]+/
 /*!
  * elasticlunr.Pipeline
  * Copyright (C) 2016 Oliver Nightingale
