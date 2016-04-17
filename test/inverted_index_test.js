784bb91fd7cc3ecdb2641c4b85b4e8f3e683d74b
weixsong <watkinsong@163.com>
2016-04-17 21:59:35 +0800

    add test cases for elasticlunr.InvertedIndex

--- test/inverted_index_test.js
+++ test/inverted_index_test.js
@@ -19,6 +19,21 @@ test('adding a token to the invertedIndex', function () {
   equal(invertedIndex.getTermFrequency('foo', 123), 1);
 });
 
+test('test hasToken() function', function () {
+  var invertedIndex = new elasticlunr.InvertedIndex,
+      doc = { ref: 123, tf: 1},
+      token = 'foo';
+
+  invertedIndex.addToken(token, doc);
+  ok(invertedIndex.hasToken(token));
+  ok(invertedIndex.hasToken('fo'));
+  ok(invertedIndex.hasToken('f'));
+
+  ok(!invertedIndex.hasToken('bar'));
+  ok(!invertedIndex.hasToken('foo '));
+  ok(!invertedIndex.hasToken('foo  '))
+});
+
 test('adding another document to the token', function () {
   var invertedIndex = new elasticlunr.InvertedIndex,
       doc1 = { ref: 123, tf: 1},
@@ -78,9 +93,10 @@ test('checking if a token does not exist in the invertedIndex', function () {
       doc = { ref: 123, tf: 1},
       token = 'foo';
 
-  ok(!invertedIndex.hasToken('bar'));
   invertedIndex.addToken(token, doc);
+  ok(!invertedIndex.hasToken('fooo'));
   ok(!invertedIndex.hasToken('bar'));
+  ok(!invertedIndex.hasToken('fof'));
 });
 
 test('retrieving items from the invertedIndex', function () {
@@ -94,6 +110,22 @@ test('retrieving items from the invertedIndex', function () {
   });
 
   deepEqual(invertedIndex.getDocs(''), {});
+
+  invertedIndex.addToken('boo', { ref: 234, tf: 100 });
+  invertedIndex.addToken('too', { ref: 345, tf: 101 });
+
+  deepEqual(invertedIndex.getDocs(token), {
+    '123': {tf: 1}
+  });
+
+  invertedIndex.addToken(token, {ref: 234, tf: 100});
+  invertedIndex.addToken(token, {ref: 345, tf: 101});
+
+  deepEqual(invertedIndex.getDocs(token), {
+    '123': {tf: 1},
+    '234': {tf: 100},
+    '345': {tf: 101}
+  });
 });
 
 test('retrieving items that do not exist in the invertedIndex', function () {
@@ -116,6 +148,10 @@ test('test df of items in the invertedIndex', function () {
   equal(invertedIndex.getDocFreq('foo'), 2);
   equal(invertedIndex.getDocFreq('bar'), 1);
   equal(invertedIndex.getDocFreq('baz'), 0);
+  equal(invertedIndex.getDocFreq('ba'), 0);
+  equal(invertedIndex.getDocFreq('b'), 0);
+  equal(invertedIndex.getDocFreq('fo'), 0);
+  equal(invertedIndex.getDocFreq('f'), 0);
 });
 
 test('removing a document from the token invertedIndex', function () {
@@ -132,6 +168,7 @@ test('removing a document from the token invertedIndex', function () {
   invertedIndex.removeToken('foo', 123);
   deepEqual(invertedIndex.getDocs('foo'), {});
   equal(invertedIndex.getDocFreq('foo'), 0);
+  equal(invertedIndex.hasToken('foo'), true);
 });
 
 test('removing a document that is not in the invertedIndex', function () {
@@ -167,6 +204,15 @@ test('expand a token into all descendent tokens', function () {
   invertedIndex.addToken('bar', doc);
 
   var tokens = invertedIndex.expandToken('hel');
+  deepEqual(tokens, ['hell', 'hello', 'help', 'held']);
+
+  tokens = invertedIndex.expandToken('hell');
+  deepEqual(tokens, ['hell', 'hello']);
+
+  tokens = invertedIndex.expandToken('he');
+  deepEqual(tokens, ['hell', 'hello', 'help', 'held']);
+
+  tokens = invertedIndex.expandToken('h');
   deepEqual(tokens, ['hell', 'hello', 'help', 'held']);
 });
 
@@ -215,6 +261,7 @@ test('test get term frequency from inverted index', function () {
 
   equal(invertedIndex.getTermFrequency(token, 123), 2);
   equal(invertedIndex.getTermFrequency(token, 456), 3);
+  equal(invertedIndex.getTermFrequency(token, 789), 0);
 });
 
 test('test get term frequency from inverted index by non-exist token', function () {
@@ -239,8 +286,9 @@ test('test get term frequency from inverted index by non-exist docRef', function
   invertedIndex.addToken(token, doc1);
   invertedIndex.addToken(token, doc2);
 
-  equal(invertedIndex.getTermFrequency(token, 1), 0);
-  equal(invertedIndex.getTermFrequency(token, 2), 0);
+  equal(invertedIndex.getTermFrequency(token, 12), 0);
+  equal(invertedIndex.getTermFrequency(token, 23), 0);
+  equal(invertedIndex.getTermFrequency(token, 45), 0);
 });
 
 test('test get term frequency from inverted index by non-exist token and non-exist docRef', function () {
@@ -254,6 +302,7 @@ test('test get term frequency from inverted index by non-exist token and non-exi
 
   equal(invertedIndex.getTermFrequency('token', 1), 0);
   equal(invertedIndex.getTermFrequency('abc', 2), 0);
+  equal(invertedIndex.getTermFrequency('fo', 123), 0);
 });
 
 test('serialisation', function () {
